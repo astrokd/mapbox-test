181b5442c345fc656eed7dfa849730c13fd65e72
Kevin Dreyer <kdreyerinseattle@gmail.com>
2022-01-07 12:01:59 -0800

    setup env

--- use-mapbox-gl-js-with-react/src/App.js
+++ use-mapbox-gl-js-with-react/src/App.js
@@ -1,32 +1,27 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-require('dotenv').config()
+const apiKey = process.env.REACT_APP_API_KEY1
 
-const apiKey = process.env.API_KEY1
-
-mapboxgl.accessToken = apiKey;
-
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
-
-useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-});
-
-
+mapboxgl.accessToken = apiKey
 
 
 export default function App() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+    
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
     return (
         <div>
           <div ref={mapContainer} className="map-container" />
