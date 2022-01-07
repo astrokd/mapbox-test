ea60c2925e4d648aa7fe17a8eba66ca65701f598
Kevin Dreyer <kdreyerinseattle@gmail.com>
2022-01-07 11:16:28 -0800

    setup App.js

---
+++ use-mapbox-gl-js-with-react/src/App.js
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+require('dotenv').config()
+
+const apiKey = process.env.API_KEY1
+
+mapboxgl.accessToken = apiKey;
+
+const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-70.9);
+const [lat, setLat] = useState(42.35);
+const [zoom, setZoom] = useState(9);
+
+useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+});
+
+
+
+
+export default function App() {
+    return (
+        <div>
+          <div ref={mapContainer} className="map-container" />
+        </div>
+      );
+}
